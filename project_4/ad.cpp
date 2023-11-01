01648b04c88ff3b2cc4cc6364d6c2439d13764c7
DIMAS <dimaromashov393@gmail.com>
2023-11-01 19:05:56 +0300

    fix project_4

--- project_4/ad.cpp
+++ project_4/ad.cpp
@@ -1,51 +1,27 @@
 #include "ad.h"
 
+int main(){
 
+std::fstream file;
+file.open("in.txt");
+int amount = 0;
+file>>amount;
+Information inf[amount];
 
+for(int i = 0; i < amount; ++i){
+    inf[i].set_information(file);
+}
+for(int i = amount - 1; i > -1; i--){
+    std::cout<<inf[i].get_full_adress()<<std::endl;
+}
 
-int main()
-{
+std::sort(inf, inf + amount, [] (Information& ps, Information& gs){return ps.get_city_name() < gs.get_city_name() ? true : false;});
+file.close();
 
-    std::ifstream fs;
-    fs.open("in.txt");
-    int amount_of_adres;
-    fs>>amount_of_adres;
-    Information* inf = new Information[amount_of_adres];
-
-
-    for(int i = 0; i < amount_of_adres; i++){
-        std::string cringe_one;
-        std::string cringe_two;
-        int cringe_tree;
-        int cringe_four;
-        fs>>cringe_one;
-        fs>>cringe_two;
-        fs>>cringe_tree;
-        fs>>cringe_four;
-
-        inf[i].set_adres(cringe_one,cringe_two,cringe_tree,cringe_four);
-        inf[i].all_to_str();
-    }
-    fs.close();
-
-    std::ofstream out;
-    out.open("out.txt");
-    out<<"ДО СОРТИРОВКИ"<<std::endl;
-    out<<amount_of_adres<<std::endl;
-
-    for(int i = amount_of_adres - 1; i > -1; i--){
-    std::string hello = inf[i].get_full_adres();
-    out<<hello<<std::endl;
-    }
-
-    out<<"После сортировки по адресу города"<<std::endl;
-    out<<"|||||||||||||||||||||||||||||||||"<<std::endl;
-    std::sort(inf,inf + amount_of_adres,[](Information& a, Information& b){return a.get_full_adres() > b.get_full_adres()? true:false; });
-
-    for(int i = amount_of_adres - 1; i > -1; i--){
-    std::string hello = inf[i].get_full_adres();
-    out<<hello<<std::endl;
-    }
-    out.close();
-    delete [] inf;
+std::ofstream out;
+out.open("out.txt");
+for(int i = 0; i < amount; ++i){
+out<<inf[i].get_full_adress()<<std::endl;
+}
+out.close();
 }
--- project_4/ad.h
+++ project_4/ad.h
@@ -4,46 +4,52 @@
 #include <fstream>
 #include <algorithm>
 
+
 class Information{
 	std::string city;
 	std::string street;
-	int number_of_home;
-	int number_of_flat;
-	std::string full_adres;
+	int house;
+	int flat_number;
 public:
 	Information();
-	void all_to_str();
-	std::string get_full_adres(){
-		return full_adres;
-	}
-	void set_adres(std::string city,std::string street,int number_of_home,int number_of_flat);
+	std::string get_full_adress();
+	void set_information(std::fstream& file);
+	std::string get_city_name();
 };
 
 Information::Information()
 {
-	this->city = "";
-	this->street = "";
-	this->number_of_home = 0;
-	this->number_of_flat = 0;
+	city = "";
+	street = "";
+	house = 0;
+	flat_number = 0;
 }
 
-void Information::all_to_str()
+
+void Information::set_information(std::fstream& file)
 {
-	full_adres += city;
-	full_adres += ' ';
-	full_adres += street;
-	full_adres += ' ';
-	full_adres += std::to_string(number_of_home);
-	full_adres += ' ';
-	full_adres += std::to_string(number_of_flat);
+	file>>city;
+	file>>street;
+	file>>house;
+	file>>flat_number;
 }
 
-void Information::set_adres(std::string city,std::string street,int number_of_home,int number_of_flat)
+std::string Information::get_city_name()
 {
-	this->city = city;
-	this->street = street;
-	this->number_of_home = number_of_home;
-	this->number_of_flat = number_of_flat;
+	return city;
 }
+
+std::string Information::get_full_adress()
+{
+	std::string full_adress;
+	full_adress += city += " ";
+	full_adress += street += " ";
+	std::string buf = std::to_string(house);
+	full_adress += buf += " ";
+	buf = std::to_string(flat_number);
+	full_adress += buf;
+	return full_adress;
+}
+
 
 
